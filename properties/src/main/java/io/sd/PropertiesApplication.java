b3d3cbff47e1edbebf1c86eb9341d15316f2fdb3
Shashank Dixit <shashanksdixit@gmail.com>
2019-11-11 14:23:16 +0530

    some minor modifications

--- properties/src/main/java/io/sd/PropertiesApplication.java
+++ properties/src/main/java/io/sd/PropertiesApplication.java
@@ -3,6 +3,7 @@ package io.sd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Comments 
 @SpringBootApplication
 public class PropertiesApplication {
 
